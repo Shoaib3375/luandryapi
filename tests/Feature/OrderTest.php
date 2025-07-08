be7bd1ba7c084e8f0ddbfc77621bc11e3fa4625e
Md Shoaib Islam <mestabd60@gmail.com>
2025-07-08 22:25:26 +0600

    feat: unitTest update

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -14,7 +14,6 @@ class OrderTest extends TestCase
     /**
      * A basic feature test example.
      */
-
     public function test_user_can_place_order()
     {
         $user = User::factory()->create();
